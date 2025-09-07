e363b0bc4a480898947857099775dcf4ca5a8f95
Sunwoo Lee <70701711+Sunnew-Lee@users.noreply.github.com>
2025-09-07 23:22:06 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<int> heap;	//min heap
+
+void reorder_topdown(int index)
+{
+	//heap size check so no over bound + return
+	int size = heap.size();
+	int _min;
+	int root{ heap[index] }, left, right;
+
+	if (size <= index * 2 + 1)
+	{
+		return;
+	}
+	else if (size == index * 2 + 2)
+	{
+		left = heap[index * 2 + 1];
+		_min = min(root, left);
+	}
+	else
+	{
+		left = heap[index * 2 + 1];
+		right = heap[index * 2 + 2];
+		_min = min(root, min(left, right));
+	}
+	
+	if (_min == root)
+	{
+		return;
+	}
+
+	else if (_min == left)
+	{
+		swap(heap[index], heap[index * 2 + 1]);
+		reorder_topdown(index * 2 + 1);
+	}
+
+	else if (_min == right)
+	{
+		swap(heap[index], heap[index * 2 + 2]);
+		reorder_topdown(index * 2 + 2);
+	}
+}
+
+void reorder_bottomup(int index)
+{
+	//check if no parent
+	if (index == 0)
+		return;
+
+	int root_idx{ index / 2 };
+	int left_idx, right_idx, _min, left, right;
+	if ((index & 1) == false)
+	{
+		//right_idx = index;
+		//left_idx = right_idx - 1;
+		--root_idx;
+	}
+
+	_min = min(heap[root_idx], heap[index]);
+
+	if (_min == heap[index])
+	{
+		swap(heap[root_idx], heap[index]);
+	}
+
+	reorder_bottomup(root_idx);
+
+	////check if no right	
+	//else
+	//{
+	//	left_idx = index;
+	//	left = heap[left_idx];
+
+	//	right_idx = left_idx + 1;
+	//	if (heap.size() == right_idx)
+	//	{
+	//		//no right
+	//		_min = min(heap[root_idx], left);
+	//	}
+	//	else
+	//	{
+	//		right = heap[right_idx];
+	//		_min = min(heap[root_idx], min(left, right));
+	//	}
+
+	//	if (_min == heap[root_idx])
+	//	{
+	//		return;
+	//	}
+
+	//	if (_min == left)
+	//	{
+	//		swap(heap[root_idx], heap[left_idx]);
+	//	}
+	//	else
+	//	{
+	//		swap(heap[root_idx], heap[right_idx]);
+	//	}
+	//	reorder_bottomup(root_idx);
+	//}
+	
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N,op;
+	cin >> N;
+
+	while (N--)
+	{
+		cin >> op;
+
+		//print&erase
+		//size-1과 swap후, 내려가면서 swap 진행.
+		if (op == 0)
+		{
+			if (heap.empty())
+			{
+				cout << 0 << '\n';
+				continue;
+			}
+
+			cout << heap[0] << '\n';
+			swap(heap[0], heap[heap.size() - 1]);
+			heap.pop_back();
+
+			if (heap.empty())
+			{
+				continue;
+			}
+			reorder_topdown(0);
+			continue;
+		}
+
+		//else insert to heap;
+		//size+1로 어디 입력해야하는지 찾고, swap 진행
+		heap.push_back(op);
+		reorder_bottomup(heap.size() - 1);
+
+	}
+
+	return 0;
+}
