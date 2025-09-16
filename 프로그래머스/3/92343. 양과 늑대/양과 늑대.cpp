b2e7f431dc0e64ce652e8ce75a5a1f189a09126b
Sunwoo Lee <70701711+Sunnew-Lee@users.noreply.github.com>
2025-09-16 15:47:30 +0900

    [level 3] Title: 양과 늑대, Time: 0.12 ms, Memory: 4.44 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -1,73 +1,53 @@
-#include <vector>
-#include <tuple>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int N;
-int MAX_SHEEP{ 1 };
+// 왼쪽 자식, 오른쪽 자식, 양/늑대 값
+int l[20], r[20], val[20];
+int n;
+int ans = 1;
+int vis[1<<17]; // vis[x] : 상태 x를 방문했는가?
 
-
-// mask first bit 1 -> root visited
-void DFS(vector<tuple<int, int, int>>& vec, int index, int sheep, int wolf, int mask)
-{
-    if (get<0>(vec[index]))
-    {
-        ++wolf;
-    }
-    else
-    {
-        ++sheep;
-    }
-
-    if (wolf >= sheep)
-        return;
-
-    MAX_SHEEP = max(sheep, MAX_SHEEP);
-
-    mask &= ~(1 << index);
-
-    if (get<1>(vec[index]) != 0)
-    {
-        mask |= (1 << get<1>(vec[index]));
-    }
-    if (get<2>(vec[index]) != 0)
-    {
-        mask |= (1 << get<2>(vec[index]));
-    }
-
-    for (int i{ 0 }; i < N; ++i)
-    {
-        if (mask & (1 << i))
-        {
-            DFS(vec, i, sheep, wolf, mask);
+// 상태에 대한 dfs 함수
+void solve(int state){
+    if(vis[state]) return;
+    vis[state] = 1;
+    // wolf : 늑대의 수, num : 전체 정점의 수
+    int wolf = 0, num = 0;
+    for(int i = 0; i < n; i++){
+        if(state & (1<<i)){
+            num++;
+            wolf+=val[i];
         }
+    }
+    // 만약 늑대가 절반 이상일 경우 방문할 수 없는 상태이니 종료
+    if(2*wolf >= num) return;
+    // 현재 state의 양의 수가 ans보다 클 경우 ans를 갱신
+    ans = max(ans,num-wolf);
+    // 이제 다음 상태로 넘어갈 시간
+    for(int i = 0; i < n; i++){
+        // i번째 비트가 꺼져있는 경우 해당 정점이 없으니 넘어감
+        if(!(state & (1<<i))) continue;
+        // 현재 보고 있는 i번째 정점의 left가 있다면
+        if(l[i] != -1)
+            solve(state | (1<<l[i]));
+        // 현재 보고 있는 i번째 정점의 right가 있다면 
+        if(r[i] != -1)
+            solve(state | (1<<r[i]));
     }
 }
 
-//info-> sheep 0, wolf 1
 int solution(vector<int> info, vector<vector<int>> edges) {
-
-    N = info.size();
-    //each node stats from root
-    vector<tuple<int, int, int>> v(N);    //info, child, child    ->if child 0 => no child
-
-    for (int i{ 0 }; i < N - 1; ++i)
-    {
-        get<0>(v[i]) = info[i];
-
-        if (get<1>(v[edges[i][0]]) == 0)
-        {
-            get<1>(v[edges[i][0]]) = edges[i][1];
-        }
-        else if (get<2>(v[edges[i][0]]) == 0)
-        {
-            get<2>(v[edges[i][0]]) = edges[i][1];
-        }
+    n = info.size();
+    fill(l,l+n,-1);
+    fill(r,r+n,-1);    
+    for(int i = 0; i < n; i++) val[i] = info[i];
+    for(int i = 0; i < n-1; i++){
+        int a = edges[i][0]; // 부모
+        int b = edges[i][1]; // 자식
+        if(l[a] == -1) l[a] = b;
+        else r[a] = b;
     }
-    get<0>(v[N - 1]) = info.back();
-
-    //(1 << get<1>(v[0])) | (1 << get<2>(v[0]))
-    DFS(v, 0, 0, 0, 1);
-
-    return MAX_SHEEP;
+    solve(1); // 0번 노드만 포함된 상태에서 dfs 시작
+    return ans;
 }
