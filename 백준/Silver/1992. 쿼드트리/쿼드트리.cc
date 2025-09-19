6b648eed817b22d0b5be9479f6f097f06e1f1f54
Sunwoo Lee <70701711+Sunnew-Lee@users.noreply.github.com>
2025-09-19 10:57:56 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,59 +1,100 @@
 #include <iostream>
-//#include <string>
+#include <vector>
 
 using namespace std;
-char arr[64][64];
-//string arr[64];
 
-bool all_same(int N, pair<int, int> start)
+int N;
+
+//2. start func -> print '(' + recur if need divide + print ')' if returned
+//	2.1. for 0~3, check if need to divide
+//	2.2. if no need to div, print 0 or 1
+void func(int div_count, int section, pair<int,int>&& start, vector<vector<char>>& v)
 {
-    int row = start.first;
-    int column = start.second;
-    char temp = arr[row][column];
+	int len = N >> div_count;
+	if (len == '1')
+	{
+		//print this num
+		cout << v[start.first][start.second];
+		return;
+	}
 
-    for (int i{ 0 }; i < N; i++)
-        for (int j{ 0 }; j < N; j++)
-        {
-            if (temp != arr[row + i][column + j])
-                return false;
-        }
+	//len>>1; + section => start point
+	// if section
+	//	0-> 0,0	x,y
+	//	1-> len>>1 , 0
+	//	2-> 0, len>>1
+	//	3-> len>>1,len>>1
 
-    return true;
-}
+	int next_len = len >> 1;
+	pair<int, int> new_start = start;	//y,x
+	switch (section)
+	{
+	case 0: break;
+	case 1: new_start.second += len; break;
+	case 2: new_start.first += len;  break;
+	case 3: new_start.first += len; new_start.second += len; break;
+	default:
+		cout << "wrong section"; break;
+	}
 
-void func(int N, pair<int, int> start)
-{
-    if (all_same(N, start))
-    {
-        cout << arr[start.first][start.second];
-    }
-    else
-    {
-        cout << '(';
-        int half = N / 2;
+	const char value = v[new_start.first][new_start.second];
+	bool need_div{ false };
 
-        for (int i{ 0 }; i < 4; i++)
-        {
-            func(half, { start.first + (i / 2) * half, start.second + (i % 2) * half });
-        }
-        cout << ')';
-    }
+	//check if need to divide
+	for (int i{ new_start.first }; i < new_start.first+len; ++i)
+	{
+		for (int j{ new_start.second }; j < new_start.second+len; ++j)
+		{
+			if (v[i][j] != value)
+			{
+				need_div = true;
+				break;
+			}
+		}
+
+		if (need_div)
+			break;
+	}
+
+	if (need_div)
+	{
+		cout << '(';
+		func(div_count + 1, 0, move(new_start), v);
+		func(div_count + 1, 1, move(new_start), v);
+		func(div_count + 1, 2, move(new_start), v);
+		func(div_count + 1, 3, move(new_start), v);
+		cout << ')';
+	}
+
+	else
+		cout << value;
 }
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int N;
-    cin >> N;
-    for (int i{ 0 }; i < N; i++)
-        for (int j{ 0 }; j < N; j++)
-        {
-            cin >> arr[i][j];
-        }
+	ios::sync_with_stdio(0);
+	cin.tie(nullptr);
 
-    func(N, { 0,0 });
+	cin >> N;
+
+	// N-> powered 2, 2^0~2^6
+	//1. cin setup
+	vector<vector<char>> arr(N, vector<char>(N, 0));
+	for (int i{ 0 }; i < N; ++i)
+	{
+		for (int j{ 0 }; j < N; ++j)
+		{
+			cin >> arr[i][j];
+		}
+	}
+
+	//2. start func -> print '(' + recur if need divide + print ')' if returned
+	func(0, 0, { 0,0 }, arr);
+	//	2.1. for 0~3, check if need to divide
+	//	2.2. if no need to div, print 0 or 1
+	
 
 
+	//func(cur power means of div numbers, section?)
 
-    return 0;
+	return 0;
 }
