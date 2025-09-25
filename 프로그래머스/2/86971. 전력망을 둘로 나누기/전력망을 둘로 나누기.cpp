e91a158142c243a4d1a8cc015641540540a5c7ea
Sunwoo Lee <70701711+Sunnew-Lee@users.noreply.github.com>
2025-09-25 11:02:19 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.23 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,15 +1,12 @@
 #include <vector>
 #include <stack>
 #include <cmath>
-#include <iostream>
 
 using namespace std;
 
-int DFS(int N, int v1,int v2, vector<vector<int>>& graph)
+int DFS(int N, vector<vector<int>>& graph)
 {
     vector<bool> visited(N+1,false);
-    //visited[0]=true;
-    
     int count{0};
     
     stack<int> s;
@@ -21,27 +18,11 @@ int DFS(int N, int v1,int v2, vector<vector<int>>& graph)
         s.pop();
         
         visited[node]=true;
-        //cout<<node<<',';
-        ++count;
-        
-        int check{0};
-        
-        //else if check needed?
-        if(node == v1)
-        {
-            check = v2;
-            //cout<<"v2"<<':'<<v2;
-        }
-        else if(node == v2)
-        {
-            check = v1;
-            //cout<<"v1"<<':'<<v1;
-        }
-        
+        ++count;               
 
         for(int n: graph[node])
         {
-            if(visited[n] || n==check)
+            if(visited[n])
             {
                 continue;
             }
@@ -49,26 +30,30 @@ int DFS(int N, int v1,int v2, vector<vector<int>>& graph)
             s.push(n);
         }
     }
-    //cout<<'\n'<<count;
-    return abs(2*count - N);
-    
+    return abs(2*count - N);    
 }
 
 int solution(int n, vector<vector<int>> wires) {
     int answer = n;
     
-    // 1.인접리스트로 그래프 구성
-    vector<vector<int>> graph(n+1); // 0번 인덱스 버림. 0,1,2,3,4
-    for(vector<int>& v : wires)
+    // 1.for each wire
+    for(vector<int>& skip_wire : wires)
     {
-        graph[v[0]].push_back(v[1]);
-        graph[v[1]].push_back(v[0]);
-    }
-    
-    // 2.for each wire DFS
-    for(vector<int>& v : wires)
-    {
-        answer = min(answer, DFS(n, v[0], v[1], graph));
+        // 2.인접리스트로 그래프 구성.
+        vector<vector<int>> graph(n+1); // 0번 인덱스 버림. 0,1,2,3,4
+        for(vector<int>& v : wires)
+        {
+            if(v[0] == skip_wire[0] && v[1] == skip_wire[1])
+            {
+                continue;
+            }
+            
+            graph[v[0]].push_back(v[1]);
+            graph[v[1]].push_back(v[0]);
+        }
+        
+        //3.DFS
+        answer = min(answer, DFS(n, graph));
     }
     
     
