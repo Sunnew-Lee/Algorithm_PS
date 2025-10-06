4f67bcba42028cce65aaef33566302ce4a447d05
Sunwoo Lee <70701711+Sunnew-Lee@users.noreply.github.com>
2025-10-07 05:55:20 +0900

    [Gold IV] Title: 최단경로, Time: 88 ms, Memory: 9184 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
 
 using namespace std;
@@ -8,56 +7,7 @@ using namespace std;
 int V, E;
 vector<pair<int, int>> adj[20001];
 int d[20001];
-//bool fix[20001];
 int INF = 200001;
-
-
-//void Dijkstra(int s)
-//{
-//	d[s] = 0;
-//	//fix[s] = true;
-//	//int index = s;
-//
-//	while (1)
-//	{
-//		//d[]에서 가장 작은 값의 인덱스를 확정시킨다.
-//		int w = INF;
-//		int index = -1;
-//		for (int i{ 1 }; i <= V; ++i)
-//		{
-//			if (fix[i])
-//				continue;
-//
-//			if (w > d[i])
-//			{
-//				w = d[i];
-//				index = i;
-//			}
-//		}
-//
-//		//모든 인덱스가 확정되면 종료한다.
-//		if (index == -1)
-//			break;
-//
-//		//이번에 찾은 놈들 중 가장 작은 w의 인덱스를 확정한다
-//		fix[index] = true;
-//		//index = next;
-//
-//		//현재 인덱스에서 갈 수 있는 곳 본다
-//		for (pair<int, int>& p : adj[index])
-//		{
-//			// 어차피 fix이면 업데이트 될 일이 없음.
-//			//if (fix[p.second])
-//			//	continue;
-//
-//			//d[] 가 더 크면 d[현재]+w로 업데이트 한다 
-//			if (d[p.second] > d[index] + p.first)
-//			{
-//				d[p.second] = d[index] + p.first;
-//			}
-//		}
-//	}
-//}
 
 void Dijkstra(int s)
 {
