174131102ee061843d49ee12957eaf16f6e6fbb0
Sunwoo Lee <70701711+Sunnew-Lee@users.noreply.github.com>
2025-10-23 19:09:57 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,35 +1,30 @@
 #include <iostream>
 
 using namespace std;
-int dp[12];
-
+int D[12];
 int main()
 {
-    ios::sync_with_stdio(false);
+    ios::sync_with_stdio(0);
     cin.tie(nullptr);
-    int N, num;
-    cin >> N;
-    dp[1] = 1; dp[2] = 2; dp[3] = 4;
 
-    for (int i{ 4 }; i < 12; i++)
-    {
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-    }
+    int T;
+    cin >> T;
 
-    while (N--)
+    D[0] = 1;
+    D[1] = 1;
+    D[2] = 2;
+    for (int i{ 3 }; i <= 11; ++i)
     {
-        cin >> num;
-        cout << dp[num] << '\n';
+        D[i] = D[i - 1] + D[i - 2] + D[i - 3];
     }
+    
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        cout << D[N]<<'\n';
+    }
+    
 
     return 0;
 }
-
-// 1
-// 
-// 1+1
-// 2
-//
-// 1+1+1 2+1 1+2 3
-// 
-// 1+1+1+1 2+1+1 1+2+1 1+1+2 1+3 3+1
