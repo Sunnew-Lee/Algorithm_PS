6af81bc87648b773451151a1f9f9e9236644393e
Sunwoo Lee <70701711+Sunnew-Lee@users.noreply.github.com>
2025-12-08 13:10:33 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int arr[1001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	arr[1] = 1;
+	arr[2] = 3;
+
+	for (int i{ 3 }; i <= n; ++i)
+	{
+		arr[i] = arr[i - 1] + 2 * arr[i - 2];
+		arr[i] %= 10007;
+	}
+
+	cout << arr[n];
+
+}
